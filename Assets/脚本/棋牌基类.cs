54f15edfc46f3746ddd47ca6623927cba345111c
西贝子游 <jiaziyou8@qq.com>
2025-04-11 19:41:58 +0800

    一次补充提交（说明增加了些啥）

---
+++ Assets/脚本/棋牌基类.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.脚本
+{
+    internal class 棋牌基类
+    {
+        private List<int> 集 = new();
+        
+        public int 集合方法()
+        {
+            集.Add(1);
+            return 0;
+        }
+    }
+}
